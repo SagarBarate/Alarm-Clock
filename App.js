8cba86d477ad69b5815c4309c60f723b52bd3f6f
Sagar Barate <sagarbarate2907@com.com>
2023-05-13 13:34:01 +0530

    addclock

--- App.js
+++ App.js
@@ -5,8 +5,20 @@ const currentTime= document.getElementById('currentTime');
 const alarmTime= document.getElementById('alarmTime');
 const setBotton = document.getElementById('set-btn');
 
+const updateCurrentTime = () => {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    console.log("Time", hours, minutes, seconds);
+}
 
+updateCurrentTime();
 
+setInterval(() => {
+    // updateCurrentTime(); 
+    // console.log('12'); 
+}, 1000);
 console.log(currentTime);
 
 
