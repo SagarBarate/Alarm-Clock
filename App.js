3c2d8d37c7aef660a6156d7d8bd5d85aa2208918
Sagar Barate <sagarbarate2907@com.com>
2023-05-13 13:16:02 +0530

    skillt

---
+++ App.js
@@ -0,0 +1,13 @@
+
+// Current time 
+
+const currentTime= document.getElementById('currentTime');
+const alarmTime= document.getElementById('alarmTime');
+const setBotton = document.getElementById('set-btn');
+
+
+
+console.log(currentTime);
+
+
+
